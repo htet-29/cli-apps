55499f6408be8b048e64da20e37e5419055e5887
Htet Hlaing Win <htethlaingwin.bsn29@gmail.com>
2025-11-03 12:45:55 +0630

    Initial Commit

---
+++ lolcat/go.mod
@@ -0,0 +1,5 @@
+module github.com/htet-29/cli-apps/lolcat
+
+go 1.25.3
+
+require syreclabs.com/go/faker v1.2.3 // indirect
---
+++ lolcat/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"syreclabs.com/go/faker"
+)
+
+func main() {
+	var phrases []string
+
+	for i := 1; i < 3; i++ {
+		phrases = append(phrases, faker.Hacker().Phrases()...)
+	}
+
+	output := strings.Join(phrases[:], "; ")
+	r, g, b := 255, 215, 0 // GOLD COLOR
+
+	for j := 0; j < len(output); j++ {
+		fmt.Printf("\033[38;2;%d;%d;%dm%c\033[0m", r, g, b, output[j])
+	}
+}
